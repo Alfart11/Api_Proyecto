b8492183fd80f8fa062cf37bc7dceac6af442a6c
Arnold Alfaro <019200997J@uandina.edu.pe>
2022-06-22 02:21:04 -0500

    main

--- schemas/administrador.schema.js
+++ schemas/administrador.schema.js
@@ -2,17 +2,24 @@ const Joi = require('joi')
 
 const id = Joi.string().uuid()
 const Correo = Joi.string().email();
+const Nombre = Joi.string().min(1).max(30);
+const Apellido = Joi.string().min(1).max(30);
 const Password = Joi.string().min(1).max(30);
+
 
 const crearAdministradorSchema = Joi.object({
   // id: id.required(),
   Correo: Correo.required(),
+  Nombre: Nombre.required(),
+  Apellido: Apellido.required(),
   Password: Password.required(),
 })
 
 const actualizarAdministradorSchema = Joi.object({
   // id: id.required(),
   Correo,
+  Nombre,
+  Apellido,
   Password,
 })
 
